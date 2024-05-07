b9b2f16053fe6efb0e49607b6c8afbc829753d60
Pereira <ydarkzinhue@gmail.com>
2024-05-07 16:31:31 -0300

    PHP

---
+++ aula1php.php
@@ -0,0 +1,24 @@
+<?php
+    echo "<h1>Olá Mundo</h1>";
+
+
+    $nome = $_POST ['nome'];
+    $email = $_POST ['email'];
+    $senha = $_POST ['senha'];
+    $aniversario = $_POST ['data'];
+    $sexo = $_POST ['sexo'];
+
+    echo '<p> SEU NOME É: </p>';
+    echo $nome;
+    echo '<p> SEU EMAIL É: </p>';
+    echo $email;
+    echo '<p> SUA SENHA É: </p>';
+    echo $senha;
+    echo '<p> SEU ANIVERSÁRIO É: </p>';
+    echo $aniversario;
+    echo '<p> SEU GÊNERO É: </p>';
+    echo $sexo;
+
+
+
+?>
