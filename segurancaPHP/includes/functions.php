ce5bd1a8edadd6565196f91054d02cdb87158457
pereira <ydarkzinhue@gmail.com>
2024-05-28 15:28:34 -0300

    updates

--- segurancaPHP/includes/functions.php
+++ segurancaPHP/includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'psl-config.php';
+include_once 'psl.config.php';
 
 function sec_session_start() {
     $session_name = 'sec_session_id'; // Estabeleça um nome personalizado para a sessão
@@ -64,7 +64,7 @@ function login($email, $password, $mysqli) {
         // a senha do usuário é enviada.
     if ($db_password == $password) {
             // A senha está correta!
-            / Obtém o string usuário-agente do usuário. 
+            // Obtém o string usuário-agente do usuário. 
             $user_browser = $_SERVER['HTTP_USER_AGENT'];
             // proteção XSS conforme imprimimos este valor.
             $user_id = preg_replace("/[^0-9]+/",  "", $user_id);
@@ -127,4 +127,86 @@ function checkbrute($user_id, $mysqli) {
         }
     }
 
+
+
+
+
+function login_check($mysqli) {
+// Verifica se todas as variáveis das sessões foram definidas;
+if (isset($_SESSION['user_id'],
+    $_SESSION['username'],
+    $_SESSION['login_string'])){
+
+    $user_id = $_SESSION['user_id'];
+    $login_string = $_SESSION['login_string'];
+    $username = $_SESSION['username']; 
+
+    //Pega a string do usuário.
+    $user_browser= $_SERVER['HTTP_USER_AGENT'];
+
+
+if ($stmt= $mysqli->prepare("SELECT password
+                             FROM members
+                             WHERE id = ? LIMIT 1")){
+
+
+        // Atribui "$user_id" ao parâmetro.
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute(); //Execute the prepared query.
+        $stmt->store_result();
+
+
+if ($stmt->num_rows == 1){
+    //Caso o usuário exista, pega variáveis a partir do resultado.
+    $stmt->bind_result($password);
+    $stmt->fetch();
+    $login_check = hash('sha512', $password . $user_browser);
+    if ($login_check == $login_string) {
+        // Logado!!
+
+        return true;
+    }else{
+        return false;
+      }
+    }else{
+        return false;
+    }
+                             } else{
+
+                                false;
+                             }
+                            }else{
+                                return false;
+                            }
+                        }
+
+
+function esc_url($url){
+    if ('' == $url){
+        return $url;
+}
+
+
+$url = preg_replace('|[^a-z0-9-~+_.?#=!&;,/:%@$\|*\'()\\x80-\\xff]|i', '', $url);
+
+$strip = array('%0d', '%0a', '%0D', '%0A');
+$url = (string) $url;
+$count = 1;
+    while ($count){
+        $url = str_replace($strip, '', $url, $count);
+    }
+
+    $url = str_replace(';//', '://', $url);
+
+    $url = htmlentities($url);
+
+    $url = str_replace('&amp;', '&#038;', $url);
+    $url = str_replace("'", '&#039;', $url);
+    if ($url[0] !== '/') {
+        // Estamos interessados somente em links relacionados provenientes de $_SERVER['PHP_SELF']
+        return '';
+    } else {
+        return $url;
+    }
+}
 ?>
