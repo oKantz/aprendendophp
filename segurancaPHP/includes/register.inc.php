6abe6b8e1f748a72b081434f3f545d7fc7db2371
Pereira <ydarkzinhue@gmail.com>
2024-06-04 16:15:22 -0300

    loginseguro

--- segurancaPHP/includes/register.inc.php
+++ segurancaPHP/includes/register.inc.php
@@ -1,67 +1,63 @@
 <?php
-
 include_once 'db_connect.php';
-
-include_once 'psl-config.php';
-
+include_once 'psl.config.php';
+ 
 $error_msg = "";
-
+ 
 if (isset($_POST['username'], $_POST['email'], $_POST['p'])) {
+    // Limpa e valida os dados passados em 
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Email inválido
         $error_msg .= '<p class="error">O endereço de email digitado não é válido</p>';
     }
-
+ 
     $password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
-
     if (strlen($password) != 128) {
-
-    $error_msg .= '<p class="error">Invalid password configuration.</p>';
+        // A senha com hash deve ter 128 caracteres.
+        // Caso contrário, algo muito estranho está acontecendo
+        $error_msg .= '<p class="error">Invalid password configuration.</p>';
     }
-
+ 
+    // O nome de usuário e a validade da senha foram conferidas no lado cliente.
+    // Não deve haver problemas nesse passo já que ninguém ganha 
+    // violando essas regras.
+    //
+ 
     $prep_stmt = "SELECT id FROM members WHERE email = ? LIMIT 1";
-
     $stmt = $mysqli->prepare($prep_stmt);
-
+ 
     if ($stmt) {
-
         $stmt->bind_param('s', $email);
         $stmt->execute();
         $stmt->store_result();
-
-    if ($stmt->num_rows == 1) {
-
-        $error_msg .= '<p class="error">A user with this email address already exists.</p>';
-
-    }
-
-} else {
-    $error_msg .= '<p class="error">Database error</p>';
-}
-
-}
-
-
-if (empty($error_msg)) {
-
-    $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(  16), TRUE));
-
-    $password = hash('sha512', $password . $random_salt);
-
-    if ($insert_stmt = $mysqli->prepare("INSERT INTO members (username, email, password, salt) VALUES (?, ?, ?, ?)")) {
-        $insert_stmt->bind_param('ssss', $username, $email, $password, $random_salt);
-
-        if (! $insert_stmt->execute()) {
-
-            header('Location: ../error.php?err=Registration failure: INSERT');
-
+ 
+        if ($stmt->num_rows == 1) {
+            // Um usuário com esse email já esixte
+            $error_msg .= '<p class="error">A user with this email address already exists.</p>';
         }
+    } else {
+        $error_msg .= '<p class="error">Database error</p>';
     }
 
-    header('Location: ./register_success.php');
+ 
+    if (empty($error_msg)) {
+        // Crie um salt aleatório
+        $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
+ 
+        // Crie uma senha com salt 
+        $password = hash('sha512', $password . $random_salt);
+ 
+        // Inserir o novo usuário no banco de dados 
+        if ($insert_stmt = $mysqli->prepare("INSERT INTO members (username, email, password, salt) VALUES (?, ?, ?, ?)")) {
+            $insert_stmt->bind_param('ssss', $username, $email, $password, $random_salt);
+            // Executar a tarefa pré-estabelecida.
+            if (! $insert_stmt->execute()) {
+                header('Location: ../error.php?err=Registration failure: INSERT');
+            }
+        }
+        header('Location: ./register_success.php');
     }
-
-
+}
--- segurancaPHP/js/forms.js
+++ segurancaPHP/js/forms.js
@@ -1,68 +1,79 @@
 function formhash(form, password) {
-
+    // Crie um novo elemento de input, o qual será o campo para a senha com hash. 
     var p = document.createElement("input");
+ 
+    // Adicione um novo elemento ao nosso formulário. 
     form.appendChild(p);
     p.name = "p";
     p.type = "hidden";
     p.value = hex_sha512(password.value);
+ 
+    // Cuidado para não deixar que a senha em texto simples não seja enviada. 
     password.value = "";
-
+ 
+    // Finalmente, envie o formulário. 
     form.submit();
 }
-
+ 
 function regformhash(form, uid, email, password, conf) {
-    if (uid.value == ''           || 
-    email.value == ''      || 
-    password.value == ''  || 
-    conf.value == '') {
-
-    alert('You must provide all the requested details. Please try again');
-
-    return false;
-}
-
-re = /^\w+$/; 
-
-if(!re.test(form.username.value)) { 
-    alert("Username must contain only letters, numbers and underscores. Please try again"); 
-    form.username.focus();
-    return false;
-}
-
-if (password.value.length <6){
-    alert('Passwords must be at least 6 characters long.  Please try again');
-    form.password.focus();
-}
-
-var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/; 
-
-if (!re.test(password.value)) {
-    alert('Passwords must contain at least one number, one lowercase and one uppercase letter.  Please try again');
-
-    return false;
-}
-
-
-if (password.value != conf.value) {
-
-    alert('Your password and confirmation do not match. Please try again');
-    form.password.focus();
-    return false;
-
-}
-
-
-var p = document.createElement("input");
-
-form.appendChild(p);
-p.name = "p";
-p.type = "hidden";
-p.value = hex_sha512(password.value);
-
-password.value = "";
-conf.value = "";
-
-form.submit();
-
-return false; 
+     // Confira se cada campo tem um valor
+    if (uid.value == ''         || 
+          email.value == ''     || 
+          password.value == ''  || 
+          conf.value == '') {
+ 
+        alert('You must provide all the requested details. Please try again');
+        return false;
+    }
+ 
+    // Verifique o nome de usuário
+ 
+    re = /^\w+$/; 
+    if(!re.test(form.username.value)) { 
+        alert("Username must contain only letters, numbers and underscores. Please try again"); 
+        form.username.focus();
+        return false; 
+    }
+ 
+    // Confira se a senha é comprida o suficiente (no mínimo, 6 caracteres)
+    // A verificação é duplicada abaixo, mas o cuidado extra é para dar mais 
+    // orientações específicas ao usuário
+    if (password.value.length < 6) {
+        alert('Passwords must be at least 6 characters long.  Please try again');
+        form.password.focus();
+        return false;
+    }
+ 
+    // Pelo menos um número, uma letra minúscula e outra maiúscula 
+    // Pelo menos 6 caracteres 
+ 
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/; 
+    if (!re.test(password.value)) {
+        alert('Passwords must contain at least one number, one lowercase and one uppercase letter.  Please try again');
+        return false;
+    }
+ 
+    // Verificar se a senha e a confirmação são as mesmas
+  if (password.value != conf.value) {
+        alert('Your password and confirmation do not match. Please try again');
+        form.password.focus();
+        return false;
+    }
+ 
+    // Crie um novo elemento de input, o qual será o campo para a senha com hash. 
+    var p = document.createElement("input");
+ 
+    // Adicione o novo elemento ao nosso formulário. 
+    form.appendChild(p);
+    p.name = "p";
+    p.type = "hidden";
+    p.value = hex_sha512(password.value);
+ 
+    // Cuidado para não deixar que a senha em texto simples não seja enviada. 
+    password.value = "";
+    conf.value = "";
+ 
+    // Finalizando, envie o formulário.  
+    form.submit();
+    return true;
 }
